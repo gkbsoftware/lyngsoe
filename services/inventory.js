8f28f03454399f8cf930e1ef22281bd856e2d052
gkbsoftware <gkbsoftware@gmail.com>
2015-08-02 14:34:57 -0400

    connected to db

--- services/inventory.js
+++ services/inventory.js
@@ -6,32 +6,33 @@ require('dotenv').load();
 var conString = process.env.DB_INFO;
 
 module.exports = {
-  createPart: function(newPart, cb) {
+  createPart: function(newPart) {
     console.log(newPart.partName)
-  }
 
-  // createUser: function(user, cb) {
-  //   pg.connect(conString, function(err, client, done) {
-  //     if (err) {
-  //       console.log(err);
-  //       throw(err);
-  //     }
-  //
-  //     // var shaObj = new jsSHA("SHA-1", "TEXT");
-  //     // shaObj.setHMACKey(process.env.SECRET_KEY, "TEXT");
-  //     // shaObj.update(user.password);
-  //     // user.password = shaObj.getHMAC("HEX");
-  //
-  //     client.query('INSERT INTO users (email_address, password, created_at, updated_at) VALUES ($1, $2, now(), now()) RETURNING id', [user.emailAddress, user.password], function(err, result) {
-  //       done();
-  //       if (err) {
-  //         console.log(err);
-  //         throw(err);
-  //       }
-  //
-  //       user.id = result.rows[0].id;
-  //       cb(user);
-  //     });
-  //   });
-  // }
+    if(newPart.hasSerialNumber) {
+      newPart.quantity = 1;
+    }
+
+    var conString = process.env.DB_INFO;
+
+    //this initializes a connection pool
+    //it will keep idle connections open for a (configurable) 30 seconds
+    //and set a limit of 20 (also configurable)
+    pg.connect(conString, function(err, client, done) {
+      if(err) {
+        return console.error('error fetching client from pool', err);
+      }
+      client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows[0].number);
+        //output: 1
+      });
+    });
+
+  }
 }

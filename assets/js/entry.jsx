8c49a9b09c4a547ad38e92c511cbbcc652711daf
gkbsoftware <gkbsoftware@gmail.com>
2015-08-02 12:18:36 -0400

    added bootstrap and jquery

--- assets/js/entry.jsx
+++ assets/js/entry.jsx
@@ -3,6 +3,10 @@ var Note = require('./Note');
 var App = require('./App');
 require('../css/style.css');
 
+var renderApp = function(){
+  React.render(<App />, document.getElementById("container"))
+}
+
 window.onload = function() {
-  React.render(<App />, document.getElementById('container'))
+  renderApp();
 }

7c4e5caeb01ab60d5b27b3ff7a776ed1e9e9b92c
Gregory Barnes <gkbsoftware@gmail.com>
2015-08-06 10:29:48 +0000

    added react-router

--- assets/js/App.jsx
+++ assets/js/App.jsx
@@ -1,3 +1,10 @@
+var Router = require('react-router');
+
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
 var React = require('react');
 var Note = require('./Note')
 var NewPart = require('./NewPart')
@@ -20,15 +27,29 @@ var App = React.createClass({
   render: function(){
     return(
       <div>
-        <html lang="en">
-          <body>
-            <Navigation />
-            <NewPart />
-          </body>
-        </html>
+        <header>
+          <ul>
+            <li><Link to="app">Home</Link></li>
+            <li><Link to="newPart">New Part</Link></li>
+          </ul>
+          {'Logged in as Greg'}
+        </header>
+      
+        <RouteHandler/>
       </div>
     )
   }
-})
+});
+
+var routes = (
+  <Route name="app" path='/' handler={App}>
+    <Route name="newPart" handler={NewPart}/>
+    <DefaultRoute handler={Navigation}/>
+  </Route>
+);
+
+Router.run(routes, function(Handler) {
+  React.render(<Handler/>, document.body);
+});
 
 module.exports = App;
